22168a80c49768cbd767ec0526f39b3e76eca3e9
bigfoodK <satcom1600@hanmail.net>
2020-05-30 15:25:55 +0900

    Bind shortcuts

--- web/src/keyboardHandler.ts
+++ web/src/keyboardHandler.ts
@@ -2,10 +2,48 @@
 
 import { dispatch } from "~StateStore/store"
 import { PlayerAction } from "~StateStore/_gen/player_action.ts"
+import { ConfigAction } from "~StateStore/_gen/config_action.ts";
+import { BarAction } from "~StateStore/_gen/bar_action.ts";
+import { ModeAction } from "~StateStore/_gen/mode_action.ts";
 
 document.onkeydown = (event) => {
-  if (event.code === 'Space') {
-    event.preventDefault();
-    dispatch(PlayerAction.togglePlay());
+  const {
+    altKey: alt,
+    ctrlKey: ctrl,
+    shiftKey: shift,
+    code,
+  } = event;
+
+  switch (code) {
+    case "KeyQ": {
+      dispatch(ModeAction.changeMode('singleNoteEdit'));
+    } break;
+
+    case "KeyW": {
+      dispatch(ModeAction.changeNoteTypeMode('up'));
+    } break;
+
+    case "KeyE": {
+      dispatch(ModeAction.changeMode('longNoteEdit'));
+    } break;
+
+    case "KeyA": {
+      dispatch(ModeAction.changeNoteTypeMode('left'));
+    } break;
+
+    case "KeyS": {
+      dispatch(ModeAction.changeNoteTypeMode('tap'));
+    } break;
+
+    case "KeyD": {
+      dispatch(ModeAction.changeNoteTypeMode('right'));
+    } break;
+
+    case "Space": {
+      event.preventDefault();
+      dispatch(PlayerAction.togglePlay());
+    } break;
+
+    default: break;
   }
 }
