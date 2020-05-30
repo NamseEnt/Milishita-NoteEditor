cd7c475c4e322f31a0878512d3019ed750a98f0b
bigfoodK <satcom1600@hanmail.net>
2020-05-30 14:40:56 +0900

    Block spacebar scrolling

--- web/src/keyboardHandler.ts
+++ web/src/keyboardHandler.ts
@@ -5,6 +5,7 @@ import { PlayerAction } from "~StateStore/_gen/player_action.ts"
 
 document.onkeydown = (event) => {
   if (event.code === 'Space') {
+    event.preventDefault();
     dispatch(PlayerAction.togglePlay());
   }
 }
